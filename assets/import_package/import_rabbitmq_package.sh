f83b3d726ed799a829e74d988dbf59a3a9e3213b
Mr.x <35421150+kuberxy@users.noreply.github.com>
2020-08-14 16:41:26 +0800

    Update import_rabbitmq_package.sh

--- assets/import_package/import_rabbitmq_package.sh
+++ assets/import_package/import_rabbitmq_package.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-set -e
 
+set -e
 UBUNTU_RELEASE=bionic
 KEY_URL="https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc"
 UPSTREAM_URL=(
@@ -11,56 +11,59 @@ REPOS=(bionic)
 COMPONENTS=(erlang-21.x rabbitmq-server-v3.7.x)
 PKGS=(erlang rabbitmq-server)
 VERS=("(>= 21.3.8.0)" "(>= 3.7.0)")
+set +e
 
 # Create local repository if they don't exist
-set +e
 aptly repo list -raw | grep "^local$" &> /dev/null
 if [[ $? -ne 0 ]]; then
+    set -e
     echo -n "Creating local repository..."
     aptly  -distribution ${UBUNTU_RELEASE} -component main \
       repo create local
     echo
+    set +e
 fi
-set -e
 
 # Import repository key
-set +e
-wget -O - ${KEY_URL} | gpg --no-default-keyring --keyring trustedkeys.gpg --import
 set -e
+wget -O - ${KEY_URL} | gpg --no-default-keyring --keyring trustedkeys.gpg --import
+set +e
 
 for i in $(seq ${#COMPONENTS[*]}); do
   i=$((i-1))
 
   # Create repository mirrors
-  set +e
   aptly mirror list -raw | grep "^${UBUNTU_RELEASE}-${COMPONENTS[$i]}$" &> /dev/null
   if [[ $? -ne 0 ]]; then
+    set -e
     aptly mirror create -architectures=amd64 \
       -force-components -filter="${PKGS[$i]} ${VERS[$i]}" -filter-with-deps \
       ${UBUNTU_RELEASE}-${COMPONENTS[$i]} ${UPSTREAM_URL[$i]} ${REPOS[*]} ${COMPONENTS[$i]}
+    set +e
   fi
-  set -e
 
   # Update repository mirrors
-  set +e
-  aptly mirror update ${UBUNTU_RELEASE}-${COMPONENTS[$i]}
   set -e
+  aptly mirror update ${UBUNTU_RELEASE}-${COMPONENTS[$i]}
+  set +e
 
   # Import mirror to local repository
-  set +e
+  set -e
   aptly repo import -architectures=amd64 -with-deps \
     ${UBUNTU_RELEASE}-${COMPONENTS[$i]} local ${PKGS[$i]}
-  set -e
+  set +e
 done
 
 # Publish/Update  local repository
-set +e
 aptly  publish list | grep ".*${UBUNTU_RELEASE}.* publishes .*local.*"
 if [[ $? -ne 0 ]]; then
+    set -e
     echo "Publish local repository..."
-    aptly -passphrase="${GPG_PASSWORD}" publish repo local 
+    aptly -passphrase="${GPG_PASSWORD}" publish repo local
+    set +e
 else
+    set -e
     echo "Update local repository..."
     aptly -passphrase="${GPG_PASSWORD}" publish update ${UBUNTU_RELEASE}
+    set +e
 fi
-set -e
